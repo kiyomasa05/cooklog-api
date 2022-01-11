3ed29fa24ee44d520ef57fa188ba27c44610cd41
kiyomasa05 <m230549@gmail.com>
2022-01-11 10:23:55 +0900

    corsのエラー対応で、domain直接追記

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins Rails.application.config.x.cors_allowed_origins
-    origins ENV["API_DOMAIN"] || ""
+    origins ENV["API_DOMAIN"] || "https://cooklog-frontend.herokuapp.com"
 
     resource "*",
       headers: :any,
@@ -16,4 +16,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 end
-
