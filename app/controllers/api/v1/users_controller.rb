559eaa5433fb312d3056434ccddd6be548e15d70
kiyomasa05 <m230549@gmail.com>
2022-01-14 11:23:30 +0900

    変更なし

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class UsersController < ApplicationController
       def create
-        @user=User.new(registrations_params)
+        @user = User.new(registrations_params)
 
         if @user.valid?
           if params[:user][:avatar]
@@ -13,18 +13,17 @@ module Api
             @user.avatar.attach(blob)
           end
           @user.save
-            login @user
-            render json: {
-              status: :created,
-              logged_in: true,
-              user: @user,
-            },methods: [:avatar_url]
-            
+          login @user
+          render json: {
+                   status: :created,
+                   logged_in: true,
+                   user: @user,
+                 }, methods: [:avatar_url]
         else
-          render json:{
-            status:500,
+          render json: {
+            status: 500,
             # エラーメッセージを全て取得し、返す
-            errors: ['登録できませんでした。']
+            errors: ["登録できませんでした。"],
           }
         end
       end
@@ -42,42 +41,41 @@ module Api
             )
             @user.avatar.attach(blob)
           end
-        @user.update(registrations_params)
+          @user.update(registrations_params)
           render json: {
             status: :updated,
             user: @user,
-          },methods: [:avatar_url]
+          }, methods: [:avatar_url]
         else
           render json: {
             status: 500,
-            errors: @user.errors.full_messages
+            errors: @user.errors.full_messages,
           }
           # binding.pry
         end
-#         既存のuserにavatarを添付するにはavatar.attachを呼び出します。
-# user.avatar.attach(params[:avatar])
-# avatar.attached?で特定のuserがavatarを持っているかどうかを調べられます。
-# user.avatar.attached?
+        #         既存のuserにavatarを添付するにはavatar.attachを呼び出します。
+        # user.avatar.attach(params[:avatar])
+        # avatar.attached?で特定のuserがavatarを持っているかどうかを調べられます。
+        # user.avatar.attached?
       end
 
       # お気に入りを見せる
       def show
         favorites = Favorite.where(user_id: current_user.id).pluck(:recipe_id)  # ログイン中のユーザーのお気に入りのrecipe_idカラムを取得
         @favorite_list = Recipe.find(favorites)     # Recipeテーブルから、お気に入り登録済みのレコードを取得
-        render json: 
-          @favorite_list,
+        render json: @favorite_list,
           methods: [:image_url]
       end
-    
+
       private
 
-        def registrations_params
-            params.require(:user).permit(:name,:email, :password, :password_confirmation)
-            
-        end
-        def decode(str)
-          Base64.decode64(str.split(",").last)
-        end
+      def registrations_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
+
+      def decode(str)
+        Base64.decode64(str.split(",").last)
+      end
     end
   end
 end
