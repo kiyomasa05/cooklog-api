cdc4ebe60cd980e3568738b69b469ed29a0e1ebd
kiyomasa05 <m230549@gmail.com>
2022-01-15 14:00:20 +0900

    blockされているため許可追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,6 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins Rails.application.config.x.cors_allowed_origins
     origins ENV["API_DOMAIN"] || ""
 
     resource "*",
