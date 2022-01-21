6480a05bc7f3bdd39f6072ca71296dda49a9b28e
kiyomasa05 <m230549@gmail.com>
2022-01-21 12:53:18 +0900

    protect_from_forgery with: :exceptionをコメントアウト　（エラー出たため）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::API
   skip_before_action :verify_authenticity_token, raise: false
   # Railsが認証トークンを使用しないように
   helper_method :login, :current_user
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  # 一旦シカト
 
   def login(user)
     session[:user_id] = user.id
