b6da8d3745d9a28c54f657428cc401b4a4e2fed5
kiyomasa05 <m230549@gmail.com>
2022-01-22 10:43:31 +0900

    クッキーの設定を本番環境のURL追加

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # サーバーがHTTP Cookieを処理できるような設定にする
 
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store,key:'_cooklog',
+  Rails.application.config.session_store :cookie_store,key:'_cooklog_session',
   domain: 'cooklog-frontend.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store,key:'_cooklog'
@@ -9,7 +9,7 @@ end
 
 
 if Rails.env == 'environment'
-  Rails.application.config.session_store :cookie_store,key:'_cooklog',
+  Rails.application.config.session_store :cookie_store,key:'_cooklog_session',
   domain:'localhost:3001'
 else
   Rails.application.config.session_store :cookie_store,key:'_cooklog'
